ba9e516c0a00c5f87647a8649a1913454760310b
Florin Popescu <florin@ltsdevelopment.com>
2017-12-01 09:34:27 -0300

    Fixed missing setters/getters for "name" field in Enroll Contract

--- src/Unloq/Api/Contracts/Enrollment/Enroll.php
+++ src/Unloq/Api/Contracts/Enrollment/Enroll.php
@@ -31,4 +31,24 @@ class Enroll extends UnloqModel {
 
         return $this;
     }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return $this
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
 }
